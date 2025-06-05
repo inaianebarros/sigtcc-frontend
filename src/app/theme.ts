f2bfd7639a05df35af33d4b3ad46494ea7c6a17f
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-04 22:41:08 -0300

    feat: Adicionar cores e fonts ao theme do projeto
    Issue: TCCF-1

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,5 +1,41 @@
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme();
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#2A382A',
+        },
+        secondary: {
+            main: '#D9D9D9',
+        },
+    },
+    typography: {
+        fontFamily: 'inherit, Raleway',
+        h1: {
+            fontFamily: 'inherit',
+        },
+        h2: {
+            fontFamily: 'inherit',
+        },
+        h3: {
+            fontFamily: 'inherit',
+        },
+        h4: {
+            fontFamily: 'var(--font-raleway)',
+        },
+        h5: {
+            fontFamily: 'inherit',
+        },
+        h6: {
+            fontFamily: 'inherit',
+        },
+        body1: {
+            fontFamily: 'inherit',
+        },
+        body2: {
+            fontFamily: 'inherit',
+        },
+    },
+});
 
 export default theme;
