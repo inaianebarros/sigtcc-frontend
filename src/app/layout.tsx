ddfec43ead8a4b54d1d3e1a58f089f0f5c178565
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-04 22:39:01 -0300

    feat: Adicionar fontes necessárias
    Issue: TCCF-1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,22 @@
 'use client';
 
 import { ThemeProvider } from '@mui/material/styles';
+import { Inria_Sans, Raleway } from 'next/font/google';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
+
+
+const inriaSans = Inria_Sans({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const _ = Raleway({
+  weight: ['300', '400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -10,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={inriaSans.className}>
       <body>
         <ThemeProvider theme={theme}>
           <CssBaseline />
