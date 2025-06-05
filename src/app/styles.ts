28f9e7b4c7179a9dd4fd6499189fec6486667a7e
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-04 22:41:52 -0300

    feat: Criar constante de estilo de TextField customizado
    Issue: TCCF-1

---
+++ src/app/styles.ts
@@ -0,0 +1,14 @@
+export const textFieldStyle = {
+    style: {
+        '& .MuiFilledInput-root': {
+            backgroundColor: 'transparent',
+            '&:hover': {
+                backgroundColor: 'transparent',
+            },
+            '&.Mui-focused': {
+                backgroundColor: 'transparent'
+            }
+        }
+    },
+    fontSize: { sx: { fontSize: 20 } },
+}
