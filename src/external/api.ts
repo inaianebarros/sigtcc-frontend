2f558184e0216aab50e2a623a3f36dcf1bebb126
Inaiane Barros <inaiane.mbarros@gmail.com>
2025-06-19 13:15:25 -0300

    feat: Criar rotas de consulta de areas, institutos e filtro de professor
    Issue: TCCF-9

--- src/external/api.ts
+++ src/external/api.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
-import { baseAPI, baseUserAPI } from './constants';
+import { baseAPI, baseUserAPI, baseExpertiseAreasAPI, baseInstitutesAPI, baseProfessorsAPI } from './constants';
 
 const api = axios.create({ baseURL: baseAPI });
 
 export const backendService = {
+    async getExpertiseAreas() {
+        const response = await api.get(baseExpertiseAreasAPI);
+        return response.data;
+    },
+    async getInstitutes() {
+        const response = await api.get(baseInstitutesAPI);
+        return response.data;
+    },
+
+    async getFilteredProfessors(expertiseAreasUUIDs: string[], institutesUUID: string, professorName: string) {
+        const response = await api.get(
+            baseProfessorsAPI, { params: { expertise_areas_uuids: expertiseAreasUUIDs, institute_uuids: institutesUUID, first_name: professorName } }
+        );
+        return response.data;
+    },
+
     async login(username: string, password: string) {
         const response = await api.post(`${baseUserAPI}/pair`, { username, password });
         const { access_token, refresh_token } = response.data;
